853729c2873c8c326ec8ee9aa5dd3b3d96e5b971
TatyanaGurtovenko <48710825+TatyanaGurtovenko@users.noreply.github.com>
2021-03-30 10:45:38 +0300

    add test task 2

---
+++ test-task-2/script.js
@@ -0,0 +1,76 @@
+let data = {
+  'Катал ог товаров': {
+    'Мойки': {
+      'Ulgran': {
+        'Smth': {},
+        'Smth': {}
+      },
+      'Vigro Mramor': {},
+      'Handmade': {
+        'Smth': {},
+        'Smth': {}
+      },
+      'Vigro Glass': {}
+    },
+    'Фильтры': {
+      'Ulgran': {
+        'Smth': {},
+        'Smth': {}
+      },
+      'Vigro Mramor': {},
+    },
+  },
+};
+
+function createTree(container, obj) {
+  container.append(createTreeDom(obj));
+}
+
+function createTreeDom(obj) {
+  if (!Object.keys(obj).length) return;
+
+  let ul = document.createElement('ul');
+  let img = document.createElement('img');
+  ul.className = 'tree-ul';
+  ul.append(img);
+
+  for (let key in obj) {
+    let li = document.createElement('li');
+    
+    li.innerHTML = key;
+    li.className = 'tree-li';
+    
+    let childUl = createTreeDom(obj[key]);
+    if (childUl) {
+      li.append(childUl);
+    }
+
+    ul.append(li);
+  }
+  img.className = 'tree-img';
+  img.src = './folder-black-18dp.svg'
+  return ul;
+}
+
+let container = document.getElementById('container');
+
+createTree(container, data);
+
+tree = document.getElementById('container');
+
+
+for (let li of tree.querySelectorAll('li')) {
+  let span = document.createElement('span');
+  li.prepend(span);
+  span.append(span.nextSibling); 
+}
+tree.onclick = function(event) {
+
+  if (event.target.tagName != 'SPAN') return;
+  
+  let childContainer = event.target.parentNode.querySelector('ul');
+
+  if (!childContainer) return;
+
+  childContainer.hidden = !childContainer.hidden;
+}
